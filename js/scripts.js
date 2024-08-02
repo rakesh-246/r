9b3f13f49088d810cb3e1c13a00b66ae6ed798b1
rakesh <64588322+rakesh-246@users.noreply.github.com>
2024-08-03 05:08:14 +0530

    js

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // Scroll animations
     const sections = document.querySelectorAll('section');
     
-    // const observer = new IntersectionObserver(entries => {
-    //     entries.forEach(entry => {
-    //         if (entry.isIntersecting) {
-    //             entry.target.classList.add('visible');
-    //         } else {
-    //             entry.target.classList.remove('visible');
-    //         }
-    //     });
-    // }, {
-    //     threshold: 0.1
-    // });
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
 
     sections.forEach(section => {
         observer.observe(section);
